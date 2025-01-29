4a88ef787c3dde88970a0d53d3ee4d72caabfbfd
zzaqwert <36064089+zza1999@users.noreply.github.com>
2025-01-29 16:33:03 +0800

    deploty test2

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/IEMS5718/",
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  base: "./",
+  // resolve: {
+  //   alias: {
+  //     '@': '/src',
+  //   },
+  // },
   plugins: [vue()]
 })
