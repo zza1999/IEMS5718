6d85bf80beabbfebce20c1558bd6f53e77ce0426
zzaqwert <36064089+zza1999@users.noreply.github.com>
2025-01-29 22:18:11 +0800

    bug fixed: route hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ProductList from '../views/ProductList.vue'
 import ProductDetail from '../views/ProductDetail.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: ProductList },
     { path: '/product/:id', component: ProductDetail, props: true }
